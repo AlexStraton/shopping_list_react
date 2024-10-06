0bf821a7c0993eedc39b33ad208b028ca697df75
Alex Straton <alexandrastraton1@gmail.com>
2024-10-06 09:35:46 +0100

    created folder structure and initialised proejct using Vite

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Shopping_List from "./components/Shopping_List";
+
+function App() {
+  return (
+    <>
+      <div>
+        <Shopping_List />
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Shopping_List.jsx
@@ -0,0 +1,12 @@
+const shoppingList = [
+  { id: 1, name: "Milk", quantity: 2 },
+  { id: 2, name: "Bread", quantity: 1 },
+  { id: 3, name: "Eggs", quantity: 12 },
+  { id: 4, name: "Butter", quantity: 1 },
+  { id: 5, name: "Apples", quantity: 6 },
+  { id: 6, name: "Orange Juice", quantity: 1 },
+];
+
+export default function Shopping_List() {
+  return shoppingList.map((item) => {});
+}
